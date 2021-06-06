f0e5ee6502805ae70094ba3d23d717e4cced1bf2
tomy-0000 <yutotom0705@gmail.com>
2021-06-06 13:49:27 +0900

    net3を追加

--- config.py
+++ config.py
@@ -2,13 +2,15 @@ dataset_config = {"seq": 14,
                   "val_test_len": 30,
                   "batch_size": 10000}
 epoch = 30000
-patience = 5
+patience = 500
 repeat_num = 2
 
-net1_config = {"input_size": 1,
-               "hidden_size": 32,
+net1_config = {"hidden_size": 32,
                "num_layers": 1}
 
-net2_config = {"input_size": 8,
+net2_config = {"hidden_size": 32,
+               "num_layers": 1}
+
+net3_config = {"weather_embedding_dim": 8,
                "hidden_size": 32,
                "num_layers": 1}
