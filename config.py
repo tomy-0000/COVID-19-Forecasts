f8e283f20f9e9c1c39f545e935eb4754a6afb2d9
tomy-0000 <yutotom0705@gmail.com>
2021-06-06 12:43:08 +0900

    init

---
+++ config.py
@@ -0,0 +1,9 @@
+dataset_config = {"seq": 14,
+                  "val_test_len": 30,
+                  "batch_size": 10000}
+epoch = 30000
+patience = 500
+
+net1_config = {"input_size": 1,
+               "hidden_size": 32,
+               "num_layers": 1}
