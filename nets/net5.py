d9851106731548183646af8adad34b07710bc900
tomy-0000 <yutotom0705@gmail.com>
2021-06-10 08:28:02 +0900

    曜日(ワンホットエンコーディング)と天候を特徴量

---
+++ nets/net5.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import torch.nn as nn
+
+class Net(nn.Module):
+    def __init__(self, hidden_size, num_layers):
+        super().__init__()
+        self.lstm = nn.LSTM(16, hidden_size, num_layers, batch_first=True)
+        self.linear = nn.Linear(hidden_size, 1)
+
+    def forward(self, x):
+        x, _ = self.lstm(x)
+        y = self.linear(x[:, -1, :])
+        return y
+
+    @staticmethod
+    def get_data():
+        df1 = pd.read_csv("https://raw.githubusercontent.com/tomy-0000/COVID-19-Forecasts/master/data/count.csv", parse_dates=True, index_col=0)
+        df1["day_name"] = df1.index.day_name()
+        df1 = pd.get_dummies(df1, prefix="", prefix_sep="")
+        columns = ["count", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+        df1 = df1.loc[:, columns]
+        df2 = pd.read_csv("https://raw.githubusercontent.com/tomy-0000/COVID-19-Forecasts/master/data/weather.csv", parse_dates=True, index_col=0)
+        df = pd.concat([df1, df2], axis=1)
+        data = df.to_numpy(dtype=float)[150:]
+        return data
+
+    dataset_config = {"seq": 30,
+                      "val_test_len": 30,
+                      "batch_size": 10000,
+                      "normalization_idx": [0]}
+
+    net_config = {"hidden_size": 32,
+                  "num_layers": 1}
