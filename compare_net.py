9484ffd90164f4cab0afb66dcb509a71c9dc7158
tomy-0000 <yutotom0705@gmail.com>
2021-06-09 09:20:40 +0900

    get_data()内でcsvを読み込むように変更

--- compare_net.py
+++ compare_net.py
@@ -11,25 +11,11 @@ import utils
 import config
 import nets
 
-# url1 ="https://docs.google.com/spreadsheets/d/1Ot0T8_YZ2Q0dORnKEhcUmuYCqZ1y81PIsIAMB7WZE8g/gviz/tq?tqx=out:csv&sheet=%E7%BD%B9%E6%82%A3%E8%80%85_%E6%9D%B1%E4%BA%AC_2020"
-# url2 = "https://docs.google.com/spreadsheets/d/1V1eJM1mupE9gJ6_k0q_77nlFoRuwDuBliMLcMdDMC_E/gviz/tq?tqx=out:csv&sheet=%E7%BD%B9%E6%82%A3%E8%80%85_%E6%9D%B1%E4%BA%AC_2021"
-
-# df = pd.concat([pd.read_csv(url1), pd.read_csv(url2)])
-# df = df[["公表日", "年代", "性別"]]
-# df = df[df["公表日"].notna()]
-# start = df["公表日"].iat[0]
-# end = df["公表日"].iat[-1]
-# index = pd.date_range(start=start, end=end)
-# df2 = pd.DataFrame(0, columns=["count"], index=index)
-# for date, tmp_df in df.groupby("公表日"):
-#     df2.loc[date, "count"] += len(tmp_df)
-df = pd.read_csv("https://raw.githubusercontent.com/tomy-0000/COVID-19-Forecasts/master/data/count.csv", parse_dates=True, index_col=0)
-
 #%%
 mae_list = []
 net_name_list = []
 
-data = nets.net1.get_data(df)
+data = nets.net1.get_data()
 Net = nets.net1.Net1
 net_config = config.net1_config
 train_val_test = utils.TrainValTest(data, **config.dataset_config)
@@ -43,7 +29,7 @@ result = utils.run_repeatedly(Net, net_config, train_val_test, config.epoch,
                               repeat_num=config.repeat_num)
 mae_list.append(result)
 
-data = nets.net2.get_data(df)
+data = nets.net2.get_data()
 Net = nets.net2.Net2
 net_config = config.net2_config
 train_val_test = utils.TrainValTest(data, **config.dataset_config)
@@ -57,7 +43,7 @@ result = utils.run_repeatedly(Net, net_config, train_val_test, config.epoch,
                               repeat_num=config.repeat_num)
 mae_list.append(result)
 
-data = nets.net3.get_data(df)
+data = nets.net3.get_data()
 Net = nets.net3.Net3
 net_config = config.net3_config
 train_val_test = utils.TrainValTest(data, **config.dataset_config)
