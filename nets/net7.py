b5cc1b7dd009f9b2e6eda5fded95bd027efbb0cf
tomy-0000 <yutotom0705@gmail.com>
2021-06-15 21:12:41 +0900

    rollingのwindowを14に変更

--- nets/net7.py
+++ nets/net7.py
@@ -15,7 +15,7 @@ class Net(nn.Module):
     @staticmethod
     def get_data():
         df = pd.read_csv("https://raw.githubusercontent.com/tomy-0000/COVID-19-Forecasts/master/data/count.csv", parse_dates=True, index_col=0)
-        df = df.rolling(window=7).mean()
+        df = df.rolling(window=14).mean()
         data = df.to_numpy(dtype=float)[150:]
         return data
 
