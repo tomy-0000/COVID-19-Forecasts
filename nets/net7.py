01b8bb92bfe9090a79137721ce172d078ff1f512
tomy-0000 <yutotom0705@gmail.com>
2021-06-15 21:07:53 +0900

    update

---
+++ nets/net7.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import torch.nn as nn
+
+class Net(nn.Module):
+    def __init__(self, hidden_size, num_layers):
+        super().__init__()
+        self.lstm = nn.LSTM(1, hidden_size, num_layers, batch_first=True)
+        self.linear = nn.Linear(hidden_size, 1)
+
+    def forward(self, x):
+        x, _ = self.lstm(x)
+        y = self.linear(x[:, -1, :])
+        return y
+
+    @staticmethod
+    def get_data():
+        df = pd.read_csv("https://raw.githubusercontent.com/tomy-0000/COVID-19-Forecasts/master/data/count.csv", parse_dates=True, index_col=0)
+        df = df.rolling(window=7).mean()
+        data = df.to_numpy(dtype=float)[150:]
+        return data
+
+    dataset_config = {"seq": 30,
+                      "val_test_len": 30,
+                      "batch_size": 10000,
+                      "normalization_idx": [0]}
+
+    net_config = {"hidden_size": 32,
+                  "num_layers": 1}
+
+# 特徴量 移動平均
+#   カウント
