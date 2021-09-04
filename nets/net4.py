4d70e33781da55b944dd4991a03b45903b9b5d76
tomy-0000 <yutotom0705@gmail.com>
2021-09-04 16:34:15 +0900

    新しいデータセットに対応

--- nets/net4.py
+++ nets/net4.py
@@ -14,18 +14,22 @@ class Net(nn.Module):
         return y
 
     @staticmethod
-    def get_data():
-        df1 = pd.read_csv("./data/count_tokyo.csv", parse_dates=True, index_col=0)
-        df2 = pd.read_csv("./data/weather.csv", parse_dates=True, index_col=0)
+    def get_data(i, use_seq, predict_seq):
+        df1 = pd.read_csv("data_use/count.csv", parse_dates=True, index_col=0)[["東京都"]]
+        df2 = pd.read_csv("data_use/weather.csv", parse_dates=True, index_col=0)
         df = pd.concat([df1, df2], axis=1)
-        data = df.to_numpy(dtype=float)[150:]
-        normalization_idx = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-        return data, normalization_idx\
+        data = df.values.astype(float)
+        total_seq = use_seq + predict_seq
+        train_data = data[:-2*total_seq]
+        val_data = data[-2*total_seq:-total_seq]
+        test_data = data[-total_seq:]
+        return [train_data, val_data, test_data]
 
-    net_params = [
-        ("hidden_size", [1, 2, 4, 8, 16, 32, 64, 128, 256]),
-        ("num_layers", [1, 2])
-    ]
+    normalization_idx = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    net_params = {
+        "hidden_size": [1, 2, 4, 8, 16, 32, 64, 128, 256],
+        "num_layers": [1, 2]
+    }
 
 # 特徴量
 #   カウント
