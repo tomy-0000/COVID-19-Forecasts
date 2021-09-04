0083c2c023d033cbdcd452225b6515c34257c4d9
tomy-0000 <yutotom0705@gmail.com>
2021-09-04 16:24:23 +0900

    新しいデータセットに対応

--- nets/net3.py
+++ nets/net3.py
@@ -4,36 +4,40 @@ import torch
 import torch.nn as nn
 
 class Net(nn.Module):
-    def __init__(self, weather_embedding_dim, hidden_size, num_layers, predict_seq):
+    def __init__(self, day_embedding_dim, hidden_size, num_layers, predict_seq):
         super().__init__()
-        self.weather_embedding = nn.Embedding(7, weather_embedding_dim)
-        feature_num = 1 + weather_embedding_dim
+        self.day_embedding = nn.Embedding(7, day_embedding_dim)
+        feature_num = 1 + day_embedding_dim
         self.lstm = nn.LSTM(feature_num, hidden_size, num_layers, batch_first=True)
         self.linear = nn.Linear(hidden_size, predict_seq)
 
     def forward(self, x):
-        x, weather = x[:, :, [0]], x[:, 0:, 1].long()
-        weather = self.weather_embedding(weather)
-        x = torch.cat([x, weather], dim=2)
+        x, day = x[:, :, [0]], x[:, :, 1].long()
+        day = self.day_embedding(day)
+        x = torch.cat([x, day], dim=2)
         x, _ = self.lstm(x)
         y = self.linear(x[:, -1, :])
         return y
 
     @staticmethod
-    def get_data():
-        df = pd.read_csv("./data/count_tokyo.csv", parse_dates=True, index_col=0)
+    def get_data(i, use_seq, predict_seq):
+        df = pd.read_csv("data_use/count.csv", parse_dates=True, index_col=0)[["東京都"]]
         df["day_name"] = df.index.day_name()
         le = LabelEncoder()
         df["day_name"] = le.fit_transform(df.index.day_name())
-        data = df.to_numpy(dtype=float)[150:]
-        normalization_idx = [0]
-        return data, normalization_idx
+        data = df.values.astype(float)
+        total_seq = use_seq + predict_seq
+        train_data = data[:-2*total_seq]
+        val_data = data[-2*total_seq:-total_seq]
+        test_data = data[-total_seq:]
+        return [train_data, val_data, test_data]
 
-    net_params = [
-        ("weather_embedding_dim", [1, 2, 4, 8, 16, 32, 64, 128, 256]),
-        ("hidden_size", [1, 2, 4, 8, 16, 32, 64, 128, 256]),
-        ("num_layers", [1, 2])
-    ]
+    normalization_idx = [0]
+    net_params = {
+        "day_embedding_dim": [1, 2, 4, 8, 16, 32, 64, 128, 256],
+        "hidden_size": [1, 2, 4, 8, 16, 32, 64, 128, 256],
+        "num_layers": [1, 2]
+    }
 
 # 特徴量
 #   カウント
