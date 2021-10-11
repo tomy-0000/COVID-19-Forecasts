62283f376a29ca1fa8b44bfd9e401b504792a398
tomy-0000 <yutotom0705@gmail.com>
2021-10-11 10:21:43 +0900

    完成

--- sir.py
+++ sir.py
@@ -1,13 +1,32 @@
 #%%
+# https://oku.edu.mie-u.ac.jp/~okumura/python/sir.html
+
 import matplotlib.pyplot as plt
 
-N = 126000000  # 日本の人口
-R0 = 2
-gamma = 0.2
-beta = R0 * gamma / N
+"""
+S(t): 感受性
+I(t): 感染性
+R(t): 回復・隔離
 
-S = N
-I = 1  # 最初は1人が感染
+beta: 伝達係数 beta*I(t)は感染力、すなわち感受性人口の単位あたり単位人口あたりの感染率
+gamma: 回復率・隔離率
+
+dS(t)/dt = -beta*S(t)*I(t)
+    感染した分減る
+dI(t)/dt = beta*S(t)*I(t) - gamma*I(t)
+    感染した分増える、回復した分減る
+dR(t)/dt = gamma*I(t)
+    回復した分増える
+"""
+
+N = 126000000  # 日本の人口
+beta = 0.26 / N
+gamma = 0.1
+R0 = beta / gamma
+delta = 0.25  # 検出率
+
+I = 1 / (delta * gamma)
+S = N - I
 R = 0
 
 aS = [S]
@@ -15,8 +34,21 @@ aI = [I]
 aR = [R]
 aRt = [R0]
 
+
+def calc_S(S, I, R):
+    return S - beta * S * I
+
+
+def calc_I(S, I, R):
+    return I + beta * S * I - gamma * I
+
+
+def calc_R(S, I, R):
+    return R + gamma * I
+
+
 for t in range(200):
-    S, I, R = S - beta * S * I, I + beta * S * I - gamma * I, R + gamma * I
+    S, I, R = calc_S(S, I, R), calc_I(S, I, R), calc_R(S, I, R)
     Rt = beta * S / gamma
     aS.append(S)
     aI.append(I)
