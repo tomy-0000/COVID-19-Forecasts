511ce713433faa79122adb954f43359905307d22
tomyy75 <yutotom0705@gmail.com>
2022-01-21 15:14:31 +0900

    data/rawに保存

--- data/download_data.py
+++ data/download_data.py
@@ -1,13 +1,8 @@
 #%%
 import pandas as pd
 
-df = (
-    pd.read_csv("https://covid19.mhlw.go.jp/public/opendata/newly_confirmed_cases_daily.csv", parse_dates=True)
-    .rename(columns={"Date": "date"})
-    .set_index("date", drop=True)
-)
-df.to_csv("data/use/Japan_count.csv")
-df["Tokyo"].to_csv("data/use/Tokyo_count.csv")
+df = pd.read_csv("https://covid19.mhlw.go.jp/public/opendata/newly_confirmed_cases_daily.csv")
+df.to_csv("data/raw/Japan.csv")
 
 df = pd.read_csv("https://covid.ourworldindata.org/data/owid-covid-data.csv")
-df.to_csv("data/use/World_features.csv")
+df.to_csv("data/raw/World_features.csv")
