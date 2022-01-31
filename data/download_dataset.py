661d81e8fd6617abb69a20ef1e9292a89662bfc6
tomyy75 <yutotom0705@gmail.com>
2022-02-01 07:50:32 +0900

    update

--- data/download_dataset.py
+++ data/download_dataset.py
@@ -1,8 +1,10 @@
 #%%
 import pandas as pd
 
+print("Downloading Japan")
 df = pd.read_csv("https://covid19.mhlw.go.jp/public/opendata/newly_confirmed_cases_daily.csv")
-df.to_csv("data/raw/Japan.csv")
+df.to_csv("data/raw/Japan.csv", index=False)
 
+print("Downloading World")
 df = pd.read_csv("https://covid.ourworldindata.org/data/owid-covid-data.csv")
-df.to_csv("data/raw/World_features.csv")
+df.to_csv("data/raw/World.csv", index=False)
