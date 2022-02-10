28c7339ffa72521475714543523ab7ee55a76fd4
tomyy75 <yutotom0705@gmail.com>
2022-02-10 12:39:57 +0900

    バッチサイズを受け取る

--- deep_learning/nets.py
+++ deep_learning/nets.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 
 
 class PositionalEncoding(nn.Module):
-    def __init__(self, d_model, dropout=0.1, max_len=5000):
+    def __init__(self, d_model, dropout=0.1, max_len=1024):
         super().__init__()
         self.dropout = nn.Dropout(p=dropout)
 
