29ba1a48683d1c68e007cbf3a4d4ebcc490be301
tomyy75 <yutotom0705@gmail.com>
2022-02-10 12:39:01 +0900

    lstmの実装

--- deep_learning/nets.py
+++ deep_learning/nets.py
@@ -72,15 +72,34 @@ class TransformerNet(nn.Module):
 
 
 class LSTMNet(nn.Module):
-    def __init__(self, d_model):
+    def __init__(self, d_model=512):
         super().__init__()
         self.l1 = nn.Linear(1, d_model)
         self.l2 = nn.Linear(1, d_model)
-        self.LSTM = nn.LSTM(d_model, d_model, batch_first=True)
+        self.enc_lstm = nn.LSTM(d_model, d_model, batch_first=True)
+        self.dec_lstm = nn.LSTM(d_model, d_model, batch_first=True)
         self.l3 = nn.Linear(d_model, 1)
 
     def forward(self, enc_x, dec_x):
-        enc_x = enc_x.unsqueeze(-1)
-        enc_x = self.l1(enc_x)
-        dec_x = dec_x.unsqueeze(-1)
-        dec_x = self.l2(dec_x)
+        enc_x = enc_x.unsqueeze(-1)  # [N, x_seq, 1]
+        enc_x = self.l1(enc_x)  # [N, x_seq, d_model]
+        _, hc = self.enc_lstm(enc_x)
+        dec_x = dec_x.unsqueeze(-1)  # [N, t_seq, 1]
+        dec_x = self.l2(dec_x)  # [N, t_seq, d_model]
+        y, _ = self.dec_lstm(dec_x, hc)  # [N, t_seq, d_model]
+        y = self.l3(y).squeeze(-1)  # [N, t_seq]
+        return y
+
+    def test(self, enc_x, dec_x, t_seq):
+        dec_x = dec_x[:, [0]]  # [N, 1]
+        with torch.no_grad():
+            enc_x = enc_x.unsqueeze(-1)  # [N, x_seq, 1]
+            enc_x = self.l1(enc_x)  # [N, x_seq, d_model]
+            _, hc = self.enc_lstm(enc_x)
+            for i in range(1, t_seq + 1):
+                dec_x2 = dec_x[:, [-1]].unsqueeze(-1)  # [N, 1, 1]
+                dec_x2 = self.l2(dec_x2)  # [N, 1, d_model]
+                y, hc = self.dec_lstm(dec_x2, hc)  # [N, i, d_model], ...
+                y = self.l3(y).squeeze(-1)  # [N, 1]
+                dec_x = torch.cat([dec_x, y[:, [-1]]], dim=-1)  # [N, i + 1]
+        return dec_x[:, 1:]
